91e7dd15e117d6e7081f329ef162c909141cc618
Kevin Glowacz <kevin@glowacz.info>
2009-09-15 21:14:21 -0500

    initial layout

--- apps/derailleur/english.lproj/main_page.js
+++ apps/derailleur/english.lproj/main_page.js
@@ -4,18 +4,37 @@
 // ==========================================================================
 /*globals Derailleur */
 
-// This page describes the main user interface for your application.  
+// This page describes the main user interface for your application.
 Derailleur.mainPage = SC.Page.design({
 
   // The main pane is made visible on screen as soon as your app is loaded.
-  // Add childViews to this pane for views to display immediately on page 
+  // Add childViews to this pane for views to display immediately on page
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
-    
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
-      tagName: "h1", value: "Hello World"
+    childViews: 'toolbar torrentList bottombar'.w(),
+
+    toolbar: SC.ToolbarView.design({
+      layout: { top: 0, left: 0, right: 0, height: 36 },
+      anchorLocation: SC.ANCHOR_TOP
+    }),
+
+    torrentList: SC.ScrollView.design({
+      hasHorizontalScroller: NO,
+      layout: { top: 36, bottom: 20, left: 0, right: 0 },
+      backgroundColor: 'white',
+
+      contentView: SC.ListView.design({
+        contentBinding: 'Derailleur.torrentsController.arrangedObjects',
+        selectionBinding: 'Derailleur.torrentsController.selection',
+
+        contentValueKey: "name",
+        rowHeight: 50
+      })
+    }),
+
+    bottombar: SC.ToolbarView.design({
+      layout: { bottom: 0, left: 0, right: 0, height: 20 },
+      anchorLocation: SC.ANCHOR_BOTTOM
     })
   })
 
