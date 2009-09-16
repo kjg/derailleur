c7effaf3d0feb6fb6adc45ad211283c0573d99fe
Kevin Glowacz <kevin@glowacz.info>
2009-09-15 20:58:56 -0500

    basic torrent model

---
+++ apps/derailleur/fixtures/torrent.js
@@ -0,0 +1,26 @@
+// ==========================================================================
+// Project:   Derailleur.Torrent Fixtures
+// Copyright: ©2009 Kevin Glowacz
+// ==========================================================================
+/*globals Derailleur */
+
+sc_require('models/torrent');
+
+Derailleur.Torrent.FIXTURES = [
+  { guid: 1,
+    name: "Enough Records - Nhoin - Rite of Passage",
+    status: 4,
+    totalSize: 30635793,
+    sizeWhenDone: 24049425,
+    haveValid: 2422545,
+    leftUntilDone: 21626880 },
+
+  { guid: 2,
+    name: "ubuntu-9.04-desktop-amd64.iso",
+    status: 16,
+    totalSize: 730554368,
+    sizeWhenDone: 730554368,
+    haveValid: 0,
+    leftUntilDone: 730505216 }
+
+];
---
+++ apps/derailleur/models/torrent.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   Derailleur.Torrent
+// Copyright: ©2009 Kevin Glowacz
+// ==========================================================================
+/*globals Derailleur */
+
+/** @class
+
+  A Torrent
+
+  @extends SC.Record
+  @version 0.1
+*/
+Derailleur.Torrent = SC.Record.extend(
+/** @scope Derailleur.Torrent.prototype */ {
+
+  name: SC.Record.attr(String),
+  status: SC.Record.attr(Number),
+  totalSize: SC.Record.attr(Number),
+  sizeWhenDone: SC.Record.attr(Number),
+  haveValid: SC.Record.attr(Number),
+  leftUntilDone: SC.Record.attr(Number)
+
+}) ;
