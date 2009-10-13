1f207b18aeede5b7613a06fc8098ed13441324bf
Kevin Glowacz <kevin@glowacz.info>
2009-10-12 21:01:24 -0500

    no longer need the hack. Got a fix committed upstream

--- apps/derailleur/resources/main_page.js
+++ apps/derailleur/resources/main_page.js
@@ -4,8 +4,6 @@
 // ==========================================================================
 /*globals Derailleur */
 
-sc_require('views/torrent_list_item');
-
 // This page describes the main user interface for your application.
 Derailleur.mainPage = SC.Page.design({
 
